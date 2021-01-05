66f1355afd5ada0e78096e48bedddb8bfa538911
flaskoski <felipeflaskoski@outlook.com>
2021-01-04 23:20:40 -0300

    invert function to start instance

--- EC2_start_function.py
+++ EC2_start_function.py
@@ -1,4 +1,3 @@
-"""This is sample program."""
 import logging
 import os
 import sys
@@ -14,16 +13,24 @@ HANDLER.setFormatter(logging.Formatter(FORMAT))
 LOGGER.addHandler(HANDLER)
 LOGGER.setLevel(logging.INFO)
 
+# get environment variable
 INSTANCE_ID = os.environ['INSTANCE_ID']
 
 EC2 = boto3.resource('ec2')
 EC2INSTANCE = EC2.Instance(INSTANCE_ID)
 EC2CLIENT = EC2.meta.client
 
-def ec2stop():
+
+def main(event, context):
     """
-    EC2 instance stop
+    main function
     """
+    try:
+        startEC2()
+    except Exception as error:
+        LOGGER.exception(error)
+
+def startEC2():
     response = EC2CLIENT.describe_instance_status(InstanceIds=[INSTANCE_ID])
     instance_status = response['InstanceStatuses'][0]['InstanceStatus']['Status']
     system_status = response['InstanceStatuses'][0]['SystemStatus']['Status']
@@ -31,28 +38,21 @@ def ec2stop():
     if instance_status == 'initializing' or system_status == 'initializing':
         LOGGER.info('Aborts. Currently InstanceStatus Checking')
         return
-    elif EC2INSTANCE.state['Name'] == 'running' and instance_status == 'ok' and system_status == 'ok':
-        EC2INSTANCE.stop()
-        LOGGER.info('Stop InstanceID： ' + INSTANCE_ID)
-        EC2CLIENT.get_waiter('instance_stopped').wait(
+    #check if instance is currently in the stopped state
+    elif EC2INSTANCE.state['Name'] == 'stopped':
+        EC2INSTANCE.start()
+        LOGGER.info('Start InstanceID： ' + INSTANCE_ID)
+        #wait for the EC2 instance to go to running state
+        EC2CLIENT.get_waiter('instance_running').wait(
             InstanceIds=[
                 INSTANCE_ID
             ],
             WaiterConfig={
                 'Delay': 5,  # Default: 15
-                'MaxAttempts': 30  # Default: 40
+                'MaxAttempts': 20  # Default: 40
             }
         )
         LOGGER.info("Completed!")
         return
     else:
-        LOGGER.info('Not Running InstanceID： ' + INSTANCE_ID)
-
-def handler(event, context):
-    """
-    main function
-    """
-    try:
-        ec2stop()
-    except Exception as error:
-        LOGGER.exception(error)
+        LOGGER.info('InstanceID "' + INSTANCE_ID + '" is not stopped!')
